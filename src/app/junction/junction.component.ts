c969bdfa6d8ddbc1051bf1f7a390b8bc0f149554
Gal Chen <49093465+galchzh@users.noreply.github.com>
2024-06-23 13:07:50 +0300

    implement roads with cars arrival

--- src/app/junction/junction.component.ts
+++ src/app/junction/junction.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { TrafficService } from '../helpers/traffic.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-junction',
   templateUrl: './junction.component.html',
-  styleUrls: ['./junction.component.scss']
+  styleUrls: ['./junction.component.scss'],
 })
-export class JunctionComponent {
+export class JunctionComponent implements OnInit, OnDestroy {
+  northSouthCars: number[] = [];
+  eastWestCars: number[] = [];
 
+  private destroy$ = new Subject<void>();
+
+  constructor(private trafficService: TrafficService) {}
+
+  ngOnInit() {
+    this.trafficService.northSouthTraffic$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((traffic) => {
+        this.northSouthCars = Array(traffic).fill(0);
+      });
+
+    this.trafficService.eastWestTraffic$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((traffic) => {
+        this.eastWestCars = Array(traffic).fill(0);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
