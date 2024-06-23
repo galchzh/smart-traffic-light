c6e786c00d6884dd998f36e3bcc1af591c2f1e1a
Gal Chen <49093465+galchzh@users.noreply.github.com>
2024-06-23 13:11:14 +0300

    init traffic light

--- src/app/traffic-light/traffic-light.component.ts
+++ src/app/traffic-light/traffic-light.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RoadDirection } from '../helpers/helper';
 
 @Component({
   selector: 'app-traffic-light',
   templateUrl: './traffic-light.component.html',
-  styleUrls: ['./traffic-light.component.scss']
+  styleUrls: ['./traffic-light.component.scss'],
 })
 export class TrafficLightComponent {
+  @Input() direction: RoadDirection;
 
+  constructor() {}
 }
