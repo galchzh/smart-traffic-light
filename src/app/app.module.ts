68dd29a0597c4b826ab4c01cb37d6efb8d905c24
Gal Chen <49093465+galchzh@users.noreply.github.com>
2024-06-23 12:53:36 +0300

    move routing to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { JunctionComponent } from './junction/junction.component';
 import { TrafficLightComponent } from './traffic-light/traffic-light.component';
 
+const routes: Routes = [
+  {
+    path: 'junction',
+    component: JunctionComponent,
+  },
+  { path: '', redirectTo: 'junction', pathMatch: 'full' },
+];
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    JunctionComponent,
-    TrafficLightComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, JunctionComponent, TrafficLightComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
